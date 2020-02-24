cc6e0afe3d18527675f4f01d225678cd2d00ff87
Hemaharshini Ragupathy <36833228+Harshini33@users.noreply.github.com>
2020-02-24 04:46:40 -0500

    Initial Commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDV6faQJh9ZVkgwpSI1vLM3BSuUOlgyya0",
+    authDomain: "todoapp-6b15f.firebaseapp.com",
+    databaseURL: "https://todoapp-6b15f.firebaseio.com",
+    projectId: "todoapp-6b15f",
+    storageBucket: "todoapp-6b15f.appspot.com",
+    messagingSenderId: "269754877100",
+    appId: "1:269754877100:web:d9acd4d9defe2f33a04b09"
+  }
+};
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDV6faQJh9ZVkgwpSI1vLM3BSuUOlgyya0",
+    authDomain: "todoapp-6b15f.firebaseapp.com",
+    databaseURL: "https://todoapp-6b15f.firebaseio.com",
+    projectId: "todoapp-6b15f",
+    storageBucket: "todoapp-6b15f.appspot.com",
+    messagingSenderId: "269754877100",
+    appId: "1:269754877100:web:d9acd4d9defe2f33a04b09"
+  }
+};
+
